7125d41fc322d6c76c6f06ea2f21abed453c8c33
Parth Bansal <51332291+parth-b@users.noreply.github.com>
2022-09-07 22:42:15 +0530

    Time: 380 ms (48.63%), Space: 110 MB (48.94%) - LeetHub

--- 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.cpp
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int edgeScore(vector<int>& edges) {
-        unordered_map<int, long long> u;
-        for(int i = 0; i<edges.size(); i++) {
+        vector<long long> u(edges.size());
+        for(int i = 0; i<edges.size(); i++) 
             u[edges[i]] += i;
-        }
-        int max = 0, ans = -1;
-        for(auto x: u) {
-            //cout <<x.first <<" "<<x.second<<endl;
-            if(x.second > max) {
-                max = x.second;
-                ans = x.first;
-            }else if(x.second == max) ans = min(ans, x.first);
-        }
-        return ans;
+        
+        return max_element(u.begin(), u.end()) - u.begin();
     }
 };
