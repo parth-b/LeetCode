1d2196e81df9ed4d2885113caa33c8b38714f8df
Parth Bansal <51332291+parth-b@users.noreply.github.com>
2022-09-09 21:03:06 +0530

    Time: 761 ms (12.77%), Space: 158.6 MB (15.79%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
-    void dfs(int i, int dist, vector<int>& e, vector<int>& memo) {
-    while (i != -1 && memo[i] == -1) {
-        memo[i] = dist++;
-        i = e[i];
-    }
-}
-int closestMeetingNode(vector<int>& e, int node1, int node2) {
-    int res = -1, min_dist = INT_MAX;
-    vector<int> m1(e.size(), -1), m2(e.size(), -1);
-    dfs(node1, 0, e, m1);
-    dfs(node2, 0, e, m2);
-    for (int i = 0; i < e.size(); ++i)
-        if (min(m1[i], m2[i]) >= 0 && max(m1[i], m2[i]) < min_dist) {
-            min_dist = max(m1[i], m2[i]);
-            res = i;
+    
+    void dfs(int node, vector<int> &vis, unordered_map<int, int> &m, int i,vector<int>& edges) {
+        if(node != -1 and !vis[node]) {
+            vis[node] = 1;
+            m[node] = i;
+            dfs(edges[node],vis, m, i+1, edges);
         }
-    return res;
-}
+    }
+    
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        // calculate the nodes reachable from node1 and node2
+        // save it in a map with corresponding distances.
+        // for every node in one map check in another map and keep minimizing the max dist of the two. return ans
+        int n = edges.size();
+        vector<int> vis(n,0);
+        unordered_map<int,int> map1, map2;
+        dfs(node1, vis, map1, 0, edges);
+        vector<int> v(n,0);
+        dfs(node2, v, map2, 0, edges);
+        
+        int mi = INT_MAX, ans = -1;
+        for(auto& ent : map1) {
+            if(map2.find(ent.first)!=map2.end()) {
+                int dist = max(map2[ent.first], ent.second);
+                if(dist < mi) {
+                    mi = dist;
+                    ans = ent.first;
+                }else if(dist == mi) {
+                    ans = min(ans, ent.first);
+                }
+            }
+        }
+        return ans;
+    }
 };
