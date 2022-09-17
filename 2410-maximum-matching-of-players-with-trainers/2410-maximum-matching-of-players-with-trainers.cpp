95567f43786f833d95cb337ea2f79a2c7f0cea90
Parth Bansal <51332291+parth-b@users.noreply.github.com>
2022-09-17 23:29:50 +0530

    Time: 375 ms (14.29%), Space: 76.6 MB (28.57%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
@@ -1,19 +1,26 @@
-class Solution {
+class Solution 
+{
 public:
-    int matchPlayersAndTrainers(vector<int>& p, vector<int>& t) {
-        sort(p.begin(), p.end());
-        sort(t.begin(), t.end());
-        int ans = 0;
-        int i = 0, j = 0;
-        while(i < p.size() and j<t.size()) {
-            while(j<t.size()){
-                if(t[j++] >= p[i]){
-                    ans++;
-                    break;
-                } 
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) 
+    {
+        sort(players.begin(), players.end());
+        sort(trainers.begin(), trainers.end());
+        
+        int count=0;
+        int s1=0, s2=0;
+        
+        while(s1<players.size() && s2<trainers.size())
+        {
+            if(players[s1] <= trainers[s2])
+            {
+                count++;
+                s1++;
+                
             }
-            i++;
+            s2++;
+            
         }
-        return ans ;
+        return count;
+        
     }
 };
