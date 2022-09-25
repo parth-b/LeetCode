c6e99f63ed8fb7e0af224574d511d68bb516e884
Parth Bansal <51332291+parth-b@users.noreply.github.com>
2022-09-25 14:46:20 +0530

    Time: 60 ms (14.81%), Space: 16.9 MB (48.83%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.cpp
+++ 622-design-circular-queue/622-design-circular-queue.cpp
@@ -1,61 +1,32 @@
 class MyCircularQueue {
+private:
+    vector<int> v;
+    int start = 0, len = 0;
 public:
-    vector<int> q;
-    int len;
-    int head = 0, tail = 0;
-    bool empty = true;
-    MyCircularQueue(int k) {
-        q.resize(k);
-        len = k;
-    }
-    
+    MyCircularQueue(int k): v(k) {}
     bool enQueue(int value) {
-        if(isFull()) return false;
-        if(head == tail and empty) 
-            empty = false;
-        q[tail] = value;
-        tail = (tail+1)%len;
+        if (isFull()) return false;
+        v[(start + len++) % v.size()] = value;
         return true;
     }
-    
     bool deQueue() {
-        if(isEmpty()) return false;
-        
-        head = (head+1)%len;
-        if(head == tail and !empty) 
-            empty = true;
+        if (isEmpty()) return false;
+        start = (start + 1) % v.size();
+        --len;
         return true;
     }
-    
     int Front() {
-        if(!isEmpty())
-            return q[head];
-        return -1;
+        if (isEmpty()) return -1;
+        return v[start];
     }
-    
     int Rear() {
-        if(!isEmpty())
-            return q[(tail+len-1)%len];
-        return -1;
+        if (isEmpty()) return -1;
+        return v[(start + len - 1) % v.size()];
     }
-    
     bool isEmpty() {
-        return(head == tail and empty) ;
-             
+        return !len;
     }
-    
     bool isFull() {
-        return head == tail and !empty;
+        return len == v.size();
     }
 };
-
-/**
- * Your MyCircularQueue object will be instantiated and called as such:
- * MyCircularQueue* obj = new MyCircularQueue(k);
- * bool param_1 = obj->enQueue(value);
- * bool param_2 = obj->deQueue();
- * int param_3 = obj->Front();
- * int param_4 = obj->Rear();
- * bool param_5 = obj->isEmpty();
- * bool param_6 = obj->isFull();
- */
