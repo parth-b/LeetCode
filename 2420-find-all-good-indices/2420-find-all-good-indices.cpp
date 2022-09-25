f794a22c89ddec2309465ca5ab126840aadaffbe
Parth Bansal <51332291+parth-b@users.noreply.github.com>
2022-09-25 20:35:58 +0530

    Time: 405 ms (25.00%), Space: 88.9 MB (25.00%) - LeetHub

---
+++ 2420-find-all-good-indices/2420-find-all-good-indices.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> goodIndices(vector<int>& nums, int k) {
+        vector<int> i(nums.size(), 1),d(nums.size(), 1);
+        for(int j = 1; j<nums.size(); j++) {
+            if(nums[j] <= nums[j-1]) 
+                i[j] = i[j-1]+1;
+        }
+        for(int j = nums.size()-2; j >=0; j--) {
+            if(nums[j] <= nums[j+1]) 
+                d[j] = d[j+1] +1;
+                
+        }
+        vector<int> ans;
+        
+        for(int j = k; j<nums.size()-k; j++) {
+            if(i[j-1] >= k and d[j+1] >=k)
+                ans.push_back(j);
+        }
+        return ans;
+    }
+};
