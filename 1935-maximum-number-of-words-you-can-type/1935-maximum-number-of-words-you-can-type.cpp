ed8627369be09552cc7b034b0b2a0b23d4902ba1
Parth Bansal <51332291+parth-b@users.noreply.github.com>
2022-10-01 00:45:05 +0530

    Time: 6 ms (48.72%), Space: 6.6 MB (61.02%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string broken) {
-        int can = 1;
-        unordered_map<char, int> map;
-        for(auto x : broken) map[x]++;
+        int can = 1;set<char> brok;
+        for(auto c:broken){
+            brok.insert(c);
+        }
         int ans =0;
         for(auto x :  text) {
             if(x == ' ') {
                 if(can) ans++;
                 can = 1;
             }
-            if(map[x]) can = 0;
+            if(brok.count(x)) can = 0;
         }
         if(can) ans++;
         return ans;
