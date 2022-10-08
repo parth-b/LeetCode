70c9acd8ead30e2a56b064481dfbc1981596f836
Parth Bansal <51332291+parth-b@users.noreply.github.com>
2022-10-08 20:12:22 +0530

    Time: 207 ms (40.47%), Space: 38.9 MB (45.87%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,18 +1,23 @@
 class MyCalendar {
+private:
+    set<pair<int, int>> calendar;
+
 public:
-    map<int,int>events;
-    MyCalendar() {   
-    }
     bool book(int start, int end) {
-        auto next = events.upper_bound(start);
-        if(next != events.end() && (*next).second < end)return false;
-        events.insert({end,start});
+        const pair<int, int> event{start, end};
+        const auto nextEvent = calendar.lower_bound(event);
+        if (nextEvent != calendar.end() && nextEvent->first < end) {
+            return false;
+        }
+
+        if (nextEvent != calendar.begin()) {
+            const auto prevEvent = prev(nextEvent);
+            if (prevEvent->second > start) {
+                return false;
+            }
+        }
+
+        calendar.insert(event);
         return true;
     }
 };
-
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar* obj = new MyCalendar();
- * bool param_1 = obj->book(start,end);
- */
