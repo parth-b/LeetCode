88e189a7d6077e5f27d365fb369154db9da22c6c
Parth Bansal <51332291+parth-b@users.noreply.github.com>
2022-10-09 21:49:39 +0530

    Time: 43 ms (92.29%), Space: 38.6 MB (35.93%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -11,15 +11,11 @@
  */
 class Solution {
 public:
+      unordered_set<int> s;
     bool findTarget(TreeNode* root, int k) {
-        unordered_set<int> set;
-        return dfs(root, set, k);
-    }
-    
-    bool dfs(TreeNode* root, unordered_set<int>& set, int k){
-        if(root == NULL)return false;
-        if(set.count(k - root->val))return true;
-        set.insert(root->val);
-        return dfs(root->left, set, k) || dfs(root->right, set, k);
+        if (!root) return false;
+        if (s.count(k - root->val)) return true;
+        s.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
     }
 };
