6d1d157ddd47759c2be6c69cbf2069a09d7eccce
Parth Bansal <51332291+parth-b@users.noreply.github.com>
2022-10-10 18:38:45 +0530

    Time: 3 ms (45.61%), Space: 6.2 MB (78.51%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    string breakPalindrome(string pal) {
-        int i = 0; 
-        if(pal.length() == 1) return "";
-        while(i < pal.length() and pal[i] < 'b' or (pal.length()%2  and i==pal.length()/2)) i++;
-        
-        if(i == pal.length()) pal[pal.length() - 1] = 'b';
-        else pal[i] = 'a';
-        return pal;
+    string breakPalindrome(string S) {
+        int n = S.size();
+        for (int i = 0; i < n / 2; ++i) {
+            if (S[i] != 'a') {
+                S[i] = 'a';
+                return S;
+            }
+        }
+        S[n - 1] = 'b';
+        return n < 2 ? "" : S;
     }
 };
